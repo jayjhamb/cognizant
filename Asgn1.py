00ddb43458a602cb2e4fd21a70b00e80049a732d
jayjhamb <83235281+jayjhamb@users.noreply.github.com>
2025-06-22 22:36:19 -0500

    Add files via upload

---
+++ Asgn1.py
@@ -0,0 +1,36 @@
+## Task 1
+
+name = "John"
+age = 20
+height = 6.3
+
+print(name + " is " + str(age) + " years old and " + str(height) + " feet tall.")
+
+## Task 2
+
+num1 = 10
+num2 = 17
+
+x = num1 + num2
+print("The sum of " + str(num1) + " and " + str(num2) + " is " + str(x) + ".")
+
+x = num2 - num1
+print("The difference between " + str(num2) + " and " + str(num1) + " is " + str(x) + ".")
+
+x = num1 * num2
+print("The product of " + str(num1) + " and " + str(num2) + " is " + str(x) + ".")
+
+x = num2 / num1
+print("The quotient of " + str(num2) + " divided by " + str(num1) + " is " + str(x) + ".")
+
+## Task 3
+inputUser = float(input("Enter a number: "))
+
+if inputUser > 0:
+    print("The number is positive.")
+elif inputUser < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+    
